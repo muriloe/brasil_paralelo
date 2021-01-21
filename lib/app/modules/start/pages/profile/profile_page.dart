814b2f8e0a63fbc19d220aadb88368ae40f5e780
Murilo Erhardt <murilo0121@gmail.com>
2021-01-21 15:33:14 -0300

    adding toast in profile page

--- lib/app/modules/start/pages/profile/profile_page.dart
+++ lib/app/modules/start/pages/profile/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:brasil_paralelo/app/shared/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'profile_controller.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -25,31 +26,45 @@ class _ProfilePageState extends ModularState<ProfilePage, ProfileController> {
         title: Text('Perfil'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
-            Center(
-              child: Text(
-                'Seja Membro',
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.w900, color: Colors.white),
+        child: GestureDetector(
+          onTap: () => {},
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
+              Center(
+                child: Text(
+                  'Seja Membro',
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.w900, color: Colors.white),
+                ),
               ),
-            ),
-            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-            promoCard(context, 'patriota', '10', BPTheme.btn_main, 'Me tornar Patriota!'),
-            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-            promoCard(context, 'premium', '49', BPTheme.btn_main, 'Quero ser Premium!'),
-            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-            promoCard(context, 'mecanas', '490', BPTheme.btn_gold, 'Virar um Mecenas!'),
-            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-            Text(
-              'Ja tenho conta. Logar!',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: BPTheme.btn_main),
-            ),
-            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
-          ],
+              Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+              promoCard(context, 'patriota', '10', BPTheme.btn_main, 'Me tornar Patriota!'),
+              Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+              promoCard(context, 'premium', '49', BPTheme.btn_main, 'Quero ser Premium!'),
+              Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+              promoCard(context, 'mecanas', '490', BPTheme.btn_gold, 'Virar um Mecenas!'),
+              Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+              Text(
+                'Ja tenho conta. Logar!',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: BPTheme.btn_main),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  showToast() {
+    Fluttertoast.showToast(
+        msg: 'É necessário me contratar para ter acesso a essa funcionalidade :)',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0);
   }
 
   Widget promoCard(context, String title, String price, Color btnColor, String btnText) {
@@ -77,7 +92,9 @@ class _ProfilePageState extends ModularState<ProfilePage, ProfileController> {
                             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0), side: BorderSide(color: btnColor)),
                             padding: EdgeInsets.fromLTRB(0, 13, 0, 13),
                             textColor: Colors.white,
-                            onPressed: () {},
+                            onPressed: () {
+                              showToast();
+                            },
                             child: Text(btnText)),
                       ),
                     )
